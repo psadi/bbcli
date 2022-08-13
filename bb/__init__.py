3b10e25629d55bbbba83f57d52deb57493ef1e2a
psadi <adithya3494@gmail.com>
2022-08-13 18:08:25 +0530

    author update

--- bb/__init__.py
+++ bb/__init__.py
@@ -3,6 +3,6 @@
     bb: A command line utility to manage pull requests in bitbucket
 """
 __version__ = '0.4.1'
-__author__ = 'P S, Adithya (1601688) (adithya.ps@sc.com)'
+__author__ = 'P S, Adithya (psadi) (adithya3494@gmail.com)'
 __license__ = "MIT"
 __doc__ = f"VERSION: {__version__} \nAUTHOR: {__author__} \nLICENSE: {__license__}"
