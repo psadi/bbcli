e2eb20ed2cb08aa8d4e3439ba742f3a4c6699e9a
nicanorflavier <nicanorflavier@outlook.com>
2024-04-27 00:24:41 +0930

    corrections to make it working on ubuntu jammy

--- bb/utils/api.py
+++ bb/utils/api.py
@@ -33,11 +33,9 @@ class BitbucketAPI:
     def default_reviewers(
         self, project: str, repo_id: str, from_branch: str, target: str
     ) -> str:
-        reviewer_query = f"avatarSize=32&sourceRepoId={repo_id}&sourceRefId=refs%2Fheads%2F{from_branch.replace(
-            '/', '%2F')}&targetRepoId={repo_id}&targetRefId=refs%2Fheads%2F{target.replace('/', '%2F')}"
+        reviewer_query = f"avatarSize=32&sourceRepoId={repo_id}&sourceRefId=refs%2Fheads%2F{from_branch.replace('/', '%2F')}&targetRepoId={repo_id}&targetRefId=refs%2Fheads%2F{target.replace('/', '%2F')}"
         return self._api_url(
-            f"/rest/default-reviewers/latest/projects/{
-                project}/repos/repository/reviewers?{reviewer_query}"
+            f"/rest/default-reviewers/latest/projects/{project}/repos/repository/reviewers?{reviewer_query}"
         )
 
     def pull_request_body(
@@ -81,14 +79,12 @@ class BitbucketAPI:
         self, project: str, repository: str, pr_number: str
     ) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{
-                pr_number}/changes?start=0&limit=1000&changeScope=unreviewed"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{pr_number}/changes?start=0&limit=1000&changeScope=unreviewed"
         )
 
     def pull_request_info(self, project: str, repository: str, _id: str) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{
-                repository}/pull-requests/{_id}"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{_id}"
         )
 
     def pull_request_viewer(self, role: str) -> str:
@@ -98,8 +94,7 @@ class BitbucketAPI:
 
     def current_pull_request(self, project: str, repository: str) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{
-                repository}/pull-requests"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests"
         )
 
     def whoami(self) -> str:
@@ -109,34 +104,29 @@ class BitbucketAPI:
         self, project: str, repository: str, target: int, user_id: str
     ) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{
-                target}/participants/{user_id}?avatarSize=32"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{target}/participants/{user_id}?avatarSize=32"
         )
 
     def pr_source_branch_delete_check(
         self, project: str, repository: str, _id: str, delete_source_branch: str
     ) -> str:
         return self._api_url(
-            f"/rest/pull-request-cleanup/latest/projects/{project}/repos/{repository}/pull-requests/{
-                _id}?deleteSourceRef={delete_source_branch}&retargetDependents={delete_source_branch}"
+            f"/rest/pull-request-cleanup/latest/projects/{project}/repos/{repository}/pull-requests/{_id}?deleteSourceRef={delete_source_branch}&retargetDependents={delete_source_branch}"
         )
 
     def validate_merge(self, project: str, repository: str, _id: str) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{
-                repository}/pull-requests/{_id}/merge"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/pull-requests/{_id}/merge"
         )
 
     def merge_config(self, project: str, repository: str) -> str:
         return self._api_url(
-            f"/rest/api/latest/projects/{project}/repos/{
-                repository}/settings/pull-requests"
+            f"/rest/api/latest/projects/{project}/repos/{repository}/settings/pull-requests"
         )
 
     def get_merge_info(self, project: str, repository: str, target_branch: str) -> str:
         return self._api_url(
-            f"/rest/branch-utils/latest/projects/{project}/repos/{
-                repository}/automerge/path/refs/heads/{target_branch}"
+            f"/rest/branch-utils/latest/projects/{project}/repos/{repository}/automerge/path/refs/heads/{target_branch}"
         )
 
     def pr_merge_body(
@@ -156,8 +146,7 @@ class BitbucketAPI:
 
     def pr_cleanup(self, project: str, repository: str, _id: str) -> str:
         return self._api_url(
-            f"/rest/pull-request-cleanup/latest/projects/{
-                project}/repos/{repository}/pull-requests/{_id}"
+            f"/rest/pull-request-cleanup/latest/projects/{project}/repos/{repository}/pull-requests/{_id}"
         )
 
     def pr_cleanup_body(self, delete_retarget: bool) -> str:
@@ -172,8 +161,7 @@ class BitbucketAPI:
         return [
             json.dumps({"version": version}),
             self._api_url(
-                f"/rest/git/latest/projects/{project}/repos/{
-                    repository}/pull-requests/{_id}/rebase"
+                f"/rest/git/latest/projects/{project}/repos/{repository}/pull-requests/{_id}/rebase"
             ),
         ]
 
@@ -181,8 +169,7 @@ class BitbucketAPI:
         return [
             json.dumps({"name": f"{source_branch}"}),
             self._api_url(
-                f"/rest/branch-utils/latest/projects/{
-                    project}/repos/{repository}/branches"
+                f"/rest/branch-utils/latest/projects/{project}/repos/{repository}/branches"
             ),
         ]
 
